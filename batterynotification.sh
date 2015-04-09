7412b4a5fe866c3d442b2878e6bc530375be6a1b
Sir-Batman <cl.yates.42@gmail.com>
2015-04-09 22:43:26 +0000

    Fixes #9.

--- batterynotification.sh
+++ batterynotification.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
+BATC=/sys/class/power_supply/BAT0/capacity
+BATS=/sys/class/power_supply/BAT0/status
+
 while :; do
 
-    BATC=/sys/class/power_supply/BAT0/capacity
+	SIGN=$(test "`cat $BATS`" = "Charging" && echo -n '+' || echo -n '-')
 	NUM=`cat $BATC`
-	echo "num: $NUM"
-	echo $(echo "$NUM < 5" | bc)
 
-	if [ $(echo "$NUM < 5" | bc) = "1" ];
+	if [ $SIGN = "-" ];
 	then
-		notify-send -u critical "Battery Critical" "The computer will now suspend. Please find a powersource.";
-		sleep 5;
-		./suspend.sh;
-	elif [ $(echo "$NUM < 10" | bc) = "1" ];
-	then
-		notify-send -u critical "Low Battery" "Your battery is low. Please suspend or find a powersource";
+		if [ $(echo "$NUM < 5" | bc) = "1" ];
+		then
+			notify-send -u critical "Battery Critical" "The computer will now suspend. Please find a powersource.";
+			sleep 5;
+			./suspend.sh;
+		elif [ $(echo "$NUM < 10" | bc) = "1" ];
+		then
+			notify-send -u critical "Low Battery" "Your battery is low. Please suspend or find a powersource";
+		fi
 	fi
 
 	sleep 60
