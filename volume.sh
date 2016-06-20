f0886feca4b542ad5b7c44006c91663da8d0b009
Connor Yates <cl.yates.42@gmail.com>
2016-06-20 12:32:14 -0700

    Actually support muting now...

--- volume.sh
+++ volume.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
+echo $CURRENT_STATE
 
 if [ "$1" == "up" ]; then
 	amixer -c 0 sset 'Master',0 "$2"dB+
-else
+elif [ "$1" == "down" ]; then
 	amixer -c 0 sset 'Master',0 "$2"dB-
+elif [ "$1" == "mute" ]; then
+	echo "muting"
+	if [ $CURRENT_STATE == "[on]" ]; then
+		amixer set Master mute
+	else
+		amixer set Master unmute
+		amixer set Speaker unmute
+		amixer set Headphone unmute
+	fi
+else
+	echo "No command specified";
+
 fi
