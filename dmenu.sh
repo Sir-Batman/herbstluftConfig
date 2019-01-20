234ba28c089b41f787d9913b45b6bd507c26cc5d
Connor Yates <cl.yates.42@gmail.com>
2019-01-19 16:24:15 -0800

    Changes to make this initially work on desktop at home

--- dmenu.sh
+++ dmenu.sh
@@ -1,7 +1,8 @@
-source ~/.colors
-BACK=${COLOR10}
-FORE=${COLOR15}
+#source ~/.colors
+#BACK=${COLOR10}
+#FORE=${COLOR15}
 
 #echo $BACK
 #echo $FORE
-dmenu_run -b -nb "#191919" -nf $BACK -sb ${COLOR6} -sf $FORE
+#dmenu_run -b -nb "#191919" -nf $BACK -sb ${COLOR6} -sf $FORE
+dmenu_run -b -nb "#191919" 
