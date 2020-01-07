3d351fe3eb5a49ec0d92e14ec5b09b48906aae8f
Connor Yates <yatesco@oregonstate.edu>
2020-01-07 13:42:06 -0800

    Quick background and dmenu color fixes

--- dmenu.sh
+++ dmenu.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-source ~/.colors
-BACK=${COLOR10}
-FORE=${COLOR15}
+#source ~/.colors
+#BACK=${COLOR10}
+#FORE=${COLOR15}
+BACK="#002b36"
+FORE="#dc323f"
+#FORE=${}
 
 #echo $BACK
 #echo $FORE
-dmenu_run -b -nb "#191919" -nf $BACK -sb ${COLOR6} -sf $FORE
+dmenu_run -b -nb "#191919" -nf $FORE -sb $FORE -sf "#191919"
